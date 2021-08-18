a9553420eb3647f3b90241422c06abc00fc3904e
Kyle Taylor <kyletaylored@gmail.com>
2021-08-18 01:24:01 -0500

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Pantheon Autopilot Demo
+
+A simple Drupal module that provides a block for testing Autopilot VRT.
+
+## How to use
+
+1. Create a new Drupal site, best with Umami demo profile.
+1. Install this module via Composer
+   - `composer require pantheon-se/pantheon_autopilot_demo:1.0.0`
+1. Enable Pantheon Autopilot Demo
+1. Place the new Notification block on the page somewhere.
+1. Let Autopilot run the VRT to pick up the version update and see the changed block color.
---
+++ src/Plugin/Block/NotificationBlock.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\pantheon_autopilot_demo\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a sample notification block.
+ *
+ * @Block(
+ *   id = "pantheon_autopilot_demo_notification",
+ *   admin_label = @Translation("Notification"),
+ *   category = @Translation("Pantheon Autopilot Demo"),
+ *   label_display = false
+ * )
+ */
+class NotificationBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    // Add some custom markup.
+    $content = $this->t('Check out our latest blog posts for the latest news!');
+    $markup = "<div class='container'>$content</div>";
+
+    $build['content'] = [
+      '#markup' => $markup,
+    ];
+    return $build;
+  }
+
+}
