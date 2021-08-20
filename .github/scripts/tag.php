dda5fbda1d903479c850d8554b65e544cb52fc44
Kyle Taylor <kyletaylored@gmail.com>
2021-08-20 11:01:30 -0500

    Add tagging workflow

---
+++ .github/scripts/tag.php
@@ -0,0 +1,8 @@
+<?php
+
+$tag = shell_exec('git describe --tags $(git rev-list --tags --max-count=1)');
+$tag_pieces = explode('.', $tag);
+$version = (int) array_pop($tag_pieces);
+array_push($tag_pieces, $version+1);
+$tag = implode('.', $tag_pieces);
+echo $tag;
