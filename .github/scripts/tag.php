c10591d75e416a4840c491514fa96509a2d2252f
Kyle Taylor <kyletaylored@gmail.com>
2021-08-21 14:39:34 -0500

    Testing tag update

--- .github/scripts/tag.php
+++ .github/scripts/tag.php
@@ -1,8 +1,28 @@
 <?php
 
-$tag = shell_exec('git describe --tags $(git rev-list --tags --max-count=1)');
-$tag_pieces = explode('.', $tag);
-$version = (int) array_pop($tag_pieces);
-array_push($tag_pieces, $version+1);
-$tag = implode('.', $tag_pieces);
-echo trim($tag);
+if ($github = getenv('GITHUB_CONTEXT')) {
+
+    // Extract Github Actions variables
+    $github = json_decode($github, true);
+
+    // Get tags from API
+    $opts = [
+        'http' => [
+            'method' => 'GET',
+            'header' => [
+                'User-Agent: PHP'
+            ]
+        ]
+    ];
+    $context = stream_context_create($opts);
+    $content = json_decode(file_get_contents($github['event']['repository']['tags_url'], false, $context), true);
+
+    // Extract tag and update by 1.
+    $latest_tag = $content[0]['name'];
+    $tag_pieces = explode('.', $tag);
+    $version = (int) array_pop($tag_pieces);
+    array_push($tag_pieces, $version + 1);
+    $tag = implode('.', $tag_pieces);
+    putenv('TAG=$tag');
+    echo trim($tag);
+}
